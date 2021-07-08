31cd39dfe0df121bdcc6c420c363b5c767af4742
artem_vasiliev <vincere802@gmail.com>
2021-07-08 16:13:47 +0300

    16 - reducer test + memo

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,16 @@
+export default {
+    title: 'React Memo'
+}
+
+
+export const Filter = (props:any) => {
+    return (
+        <div>{props.count}</div>
+    )
+}
+
+export const Table = (props:any) => {
+    return (
+        <div>{props.count}</div>
+    )
+}
